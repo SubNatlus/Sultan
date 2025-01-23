69f2a78a51b6e5c4ee067a85795d93e10f18eaf0
SubNatlus <sultanalobaid7@gmail.com>
2025-01-23 12:33:34 +0000

    programming theory

---
+++ euclidean_algorithm.py
@@ -0,0 +1,18 @@
+class EuclideanAlgorithm:
+    """
+    Class to encapsulate the Euclidean Algorithm.
+    """
+
+    def gcd(self, a, b):
+        """
+        Compute the greatest common divisor (GCD) of two positive integers.
+        :param a: First positive integer.
+        :param b: Second positive integer.
+        :return: GCD of a and b.
+        """
+        while b != 0:
+            temp = b
+            b = a % b
+            a = temp
+        return a
+
